aa28bd7df45bf88e312cec471c3fa6e1a4d130b5
khaled Eldoheiri <khaled@eldoheiri.com>
2024-08-22 17:48:13 +0200

    Initial commit

---
+++ hclpath.go
@@ -0,0 +1,23 @@
+package hclpath
+
+import (
+	"reflect"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+)
+
+func Query(b hcl.Body, path string) (hclsyntax.Blocks, error) {
+	body := b.(*hclsyntax.Body)
+	logger.Debug("Body received", "body", b, "type", reflect.TypeOf(b))
+	compilation, err := Compile(path)
+	if err != nil {
+		return nil, err
+	}
+	blocks := body.Blocks
+	if len(blocks) == 0 {
+		logger.Debug("No blocks in the passed body")
+		return hclsyntax.Blocks{}, nil
+	}
+	return compilation.Exec(blocks)
+}
